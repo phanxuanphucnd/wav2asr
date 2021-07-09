7cb6a077112959c9c4d9a11cace205573e46b95b
phucphan <20156248@student.hust.edu.vn>
2021-07-09 16:03:47 +0700

    update

--- arizona/utils/print_utils.py
+++ arizona/utils/print_utils.py
@@ -3,6 +3,6 @@ from arizona_asr.version import __version__
 def print_name():
     print("")
     print('\n'.join([
-        ' 🅰 🆁 🅸 🆉 🅾 🅽 🅰  🅰 🆂 🆁 _ {}'.format(__version__), 
+        ' 🅰 🆁 🅸 🆉 🅾 🅽 🅰  🅰 🆂 🆁  ({})'.format(__version__), 
         ''
     ]))
